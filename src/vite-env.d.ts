e4c36f38f94818a45b1ca9ac7497a4335b68828e
yazidzm010101 <yazidzm.developer@gmail.com>
2024-11-28 18:13:20 +0700

    feat: [mujadid] fix env, fix loading skeleton cursor style

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_EXAMPLE_API_URL: string;
+  readonly REACT_APP_EXAMPLE_API_URL: string;
   // Add other environment variables here as needed
 }
 
